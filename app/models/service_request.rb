7573232c57fd4c3315d6e79232bcc61c8b3d456f
Andrew Fomera <andrew@zerlex.net>
2015-10-11 22:49:52 -0500

    Some work on Service Requests show page and return message for nil email and phone

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -2,4 +2,20 @@ class ServiceRequest < ActiveRecord::Base
   validates_presence_of :customer_name, :address, :description
 
   scope :status, -> (status) { where(status: status) }
+
+  def customer_phone_number
+    if self.customer_phone.empty?
+      "No phone number on file"
+    else
+      self.customer_phone
+    end
+  end
+
+  def customer_email_address
+    if self.customer_email.empty?
+      "No email on file"
+    else
+      self.customer_email
+    end
+  end
 end
