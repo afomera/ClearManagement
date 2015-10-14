d2557d3e14f9447a05f0974f2842ac921af3c971
Andrew Fomera <andrew@zerlex.net>
2015-10-13 23:03:31 -0500

    Add full_name to user model so we don't have to string together first/last name every time

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
